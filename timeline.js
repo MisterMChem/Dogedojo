9a19538ae13a6319844cf01cb8865edc81fc844f
MisterMChem <mistermchem@gmail.com>
2016-06-06 17:30:06 -0400

    Add files via upload

--- timeline.js
+++ timeline.js
@@ -14,8 +14,31 @@ $(document).ready(function(){
 
 function queryDatabase(token) {
   firebase.database().ref('/Posts/').once('value').then(function(snapshot) {
-    var PostArray = snapshot.val();
+    var PostObject = snapshot.val();
     console.log(PostArray);
+    var keys = Object.keys(PostObject);
+    var currentRow;
+    for (var i = 0; i< keys.length; i++) {
+      var currentObject = PostObject[keys[i]];
+      if (i % 3 == 0) {
+        currentRow = document.createElement("div");
+        $(currentRow).addClass("row");
+        $("#contentHolder").append(currentRow);
+      }
+      var col = document.createElement("div");
+      $(col).addClass("col-lg-4");
+      var image = document.createElement("img");
+      image.src = currentObject.url;
+      $(image).addClass("contentImage");
+      var p = document.createElement("p");
+      $(p).html(currentObject.caption);
+      $(p).addClass("contentCaption");
+      $(col).append(image);
+      $(col).append(p);
+      $(currentRow).append(col);
+      //create new row on every third entry
+      //col-lg-4
+    }
     // ...
   });
 
