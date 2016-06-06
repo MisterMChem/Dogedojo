df6c3c5da0b88b8393826f8d010a4ee93be16520
MisterMChem <mistermchem@gmail.com>
2016-06-06 14:00:45 -0400

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -50,8 +50,33 @@ $(".dropdown").on("hide.bs.dropdown", function(event){
 });
 
 $("#file").on("change", function(event) {
+	selectedFile = event.target.files[0];
 	$("#uploadButton").show();
 });
+
+function uploadFile() {
+	// Create a root reference
+	var filename = selectedFile.name;
+	var storageRef = firebase.storage().ref('/dogImages/' + filename);
+	var uploadTask = storageRef.put(selectedFile);
+
+	// Register three observers:
+	// 1. 'state_changed' observer, called any time the state changes
+	// 2. Error observer, called on failure
+	// 3. Completion observer, called on successful completion
+	uploadTask.on('state_changed', function(snapshot){
+	  // Observe state change events such as progress, pause, and resume
+	  // See below for more detail
+	}, function(error) {
+	  // Handle unsuccessful uploads
+	}, function() {
+	  // Handle successful uploads on complete
+	  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+	  var downloadURL = uploadTask.snapshot.downloadURL;
+	  console.log(downloadURL);
+	});
+
+}
 
 
 
